e20f21030ebd7af5ed49b5799075b98d6c580e48
nihadamirov <nihadamirov98@gmail.com>
2025-08-16 09:25:16 -0400

    update script

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Purpose: Show the last 5 login sessions with date & time
-
-if [[ $EUID -ne 0 ]]; then 
-	echo "You can only run this script as root or with sudo."
-	exit 1
-fi
-
 last -5
