04e923fe0afe1d5ab63b5a5f2c10c01e810063f8
nihadamirov <nihadamirov98@gmail.com>
2025-08-17 01:12:41 -0400

    refactored script

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/ && !/Registry/ {gsub(/:/, ","); print}' > $1.csv
+whois $1 | awk '/^[ ]*Registrant|^[ ]*Admin|^[ ]*Tech/ && !/Registry/ {gsub(/: /, ","); print}' > $1.csv
